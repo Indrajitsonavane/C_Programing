797295def600001070dec9c2f1a9196be4605ac3
indrajitsonavane <indrajitsonavane1@gmail.com>
2025-04-07 10:19:18 -0700

    Malloc

---
+++ Dynamic_Mamory_Allocation/Malloc.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int sum = 0 ,i = 0, cnt = 0 ;
+    int *Mptr = NULL;
+
+    printf("\n Enter How many Customers => ");
+    scanf("%d",&cnt);
+
+    Mptr = (int*)malloc(cnt * sizeof(int));
+
+
+    for(i=0 ; i < cnt ; i++)
+    {
+        printf("\n Enter A Num =>");
+        scanf("%d",&Mptr[i]);
+    }
+
+    for(i=0 ; i < cnt ; i++)
+    {
+          sum = sum + Mptr[i];
+    }
+
+    free(Mptr);
+    printf("\n Sum is => %d",sum);
+
+    getch();
+    return 0;
+}
